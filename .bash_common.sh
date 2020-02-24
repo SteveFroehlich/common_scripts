9988a9eea60057a64356bc0cf1f8da97c6e5a7c4
Steve Froehlich <Steve.Froehlich@cmegroup.com>
2020-02-23 21:54:11 -0600

    add alias and fix help-alias path

--- .bash_common.sh
+++ .bash_common.sh
@@ -5,6 +5,7 @@ export PATH=$PATH:~/my-scripts/
 
 ######### Alias #########
 alias recycle='pushd ~/Desktop/recycle_bin/; rm -r *; popd;'
+alias n='nano -c'
 
 #unix
 alias lah='ls -lah'
@@ -12,32 +13,49 @@ alias proc='ps -ef'
 
 # git
 alias cb='git checkout'
+alias clone='git clone '
 alias log='git log'
 alias logst='git log --stat'
 alias commit='git commit -m '
 alias commita='git commit -a -m '
 alias add='git add'
 alias rhh='git show-ref assemblem_int --heads --hash | git reset --hard'
+alias branch-sort='git branch --sort=-committerdate' # desc
+alias branch-sort-asc='git branch --sort=committerdate'
 alias stash='git stash'
 alias pull='git pull origin '
 alias update='git pull --rebase origin '
 alias push='git push origin '
 alias fetch='git fetch'
-alias d='git diff head'
+alias stash='git stash'
 alias pm='git push origin master'
-alias sq='git merge --squash '
+alias gh='git rev-parse --short HEAD'
+alias nch='git diff --numstat'
+alias gs='git merge --squash '
+alias d='git diff head'
+alias s='git status'
 
 # bash
 alias edit-bash='open ~/.bashrc'
+alias edit-profile='open ~/.bash_profile'
 alias edit-common='open ~/common_scripts/.bash_common.sh'
-alias rb='source ~/.bashrc'  # rb --> reset bash
+alias rb='source ~/.bash_profile'  # rb --> reset bash
 alias cd-scripts='cd ~/common_scripts/my-scripts/'
 alias help-scripts='ls ~/common_scripts/my-scripts/'
 alias help-alias='grep alias ~/.bashrc ~/common_scripts/.bash_common.sh'
 
-#maven
+# maven
 alias mci='mvn clean install'
 alias mcis='mvn clean install -DskipTests'
+alias mcd='mvn clean deploy'
+alias mcds='mvn clean deploy -DskipTests'
+alias mcp='mvn clean package'
+
+# spring boot
+alias sb='mvn spring-boot:run'
+
+# network 
+alias tcp-8080='lsof -i :8080'
 
 # html pages and links
 alias radar='open https://radar.weather.gov/ridge/Conus/full_loop.php'
--- my-scripts/help-alias.sh
+++ my-scripts/help-alias.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-cat ~/.bashrc | grep alias | less
+ grep alias ~/.bashrc ~/.bash_profile ~/common_scripts/.bash_common.sh
+ 
