ef296dcf8839a2bcfa7b416877605d3605b71602
Steve Froehlich <Steve.Froehlich@cmegroup.com>
2020-03-31 09:17:04 -0500

    add comment about new virtual env not working

--- my-scripts/new-virtualenv.sh
+++ my-scripts/new-virtualenv.sh
@@ -8,5 +8,8 @@ else
   echo "No environment name given. Please supply one ie: ./new-virtualenv.sh my-env-name"
 fi
 
+# this works on wmac
+# python virtualenv-15.0.3/virtualenv.py ~/virtual-envs/py3-ansi
+
 echo "creating new env: $envName"
 virtualenv ~/.virtualenvs/$envName
