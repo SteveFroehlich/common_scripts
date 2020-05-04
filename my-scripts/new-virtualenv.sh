aba0bceeb24176b3f57b0baf7c42f2eb804d1271
Steve Froehlich <Steve.Froehlich@cmegroup.com>
2020-05-04 13:40:59 -0500

    use py3 as default for new virtual env

--- my-scripts/new-virtualenv.sh
+++ my-scripts/new-virtualenv.sh
@@ -9,7 +9,15 @@ else
 fi
 
 # this works on wmac
-# python virtualenv-15.0.3/virtualenv.py ~/virtual-envs/py3-ansi
+# python ~/virtualenv-15.0.3/virtualenv.py ~/virtual-envs/py3-ansi
 
-echo "creating new env: $envName"
-virtualenv ~/.virtualenvs/$envName
+#echo "creating new env: $envName"
+#virtualenv ~/.virtualenvs/$envName
+
+
+# py 3 impl
+python3 -m venv ~/virtual-envs/$envName
+
+echo 'activating new env: $envName'
+source ~/virtual-envs/$envName/bin/activate
+
